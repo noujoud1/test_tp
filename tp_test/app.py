c4371e8c45b23225ae3c9d162fed25a6c335ed2b
Test Etudiant <test.etudiant@2026.tn>
2026-03-09 17:15:07 +0100

    calcul de max
    ajout

--- tp_test/app.py
+++ tp_test/app.py
@@ -1,1 +1,6 @@
  </>Python data =[2,4,6] print(sum(data)) 
+
+print("moyenne:",sum(data)/len(data))</>Python print (max:,max(data)) 
+ 58b8f70 (calcul de max)
+</>Python print(min:,min(data))
+956f6a8 (ajout)
